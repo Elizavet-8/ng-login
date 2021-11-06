b3c6b08e14bc78bec90d28cb1f40abd42b3f05ec
Elizavet-8 <70907348+Elizavet-8@users.noreply.github.com>
2021-11-06 13:33:58 +0300

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { PofileComponent } from './pofile/pofile.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PofileComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+      { path: 'profile', component: PofileComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
